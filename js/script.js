271e9491127546698c3396e706fdf1df55a1f077
Aqeel <aqeel-sheikh@users.noreply.github.com>
2025-09-16 16:32:04 +0530

    feat(the-beginning-section): Implement the section background and title

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const headerTitle = document.querySelector("#header-title");
 const headerTagLine = document.querySelector("#header-tagline");
 const socialLinks = document.querySelectorAll(".social-links a");
-
+const rocket = document.querySelector(".rocket")
 
 function onScroll() {
   const scrollY = window.scrollY;
@@ -9,6 +9,7 @@ function onScroll() {
   headerTitle.style.marginBottom = `${scrollY * 0.9}px`;
 
   headerTagLine.style.marginTop = scrollY + "px";
+
 
   for (let link of socialLinks) {
     link.style.paddingBottom = `${scrollY * 0.9}px`;
