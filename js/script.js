3c48d54baba1c0356da946ef4f747ea5af0be8aa
Aqeel <aqeel-sheikh@users.noreply.github.com>
2025-09-21 14:01:24 +0530

    feat(sections): Implement Gsap scroll animation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const headerTitle = document.querySelector("#section1-title");
 const headerTagLine = document.querySelector("#section1-tagline");
 const socialLinks = document.querySelectorAll(".social-links a");
-const rocket = document.querySelector(".rocket")
+const rocket = document.querySelector(".rocket");
 
 function onScroll() {
   const scrollY = window.scrollY;
@@ -10,7 +10,6 @@ function onScroll() {
 
   headerTagLine.style.marginTop = scrollY + "px";
 
-
   for (let link of socialLinks) {
     link.style.paddingBottom = `${scrollY * 0.9}px`;
   }
@@ -18,3 +17,22 @@ function onScroll() {
 
 window.addEventListener("scroll", onScroll, { passive: true });
 onScroll();
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.defaults({ ease: "none", duration: 2 });
+
+const tl = gsap.timeline();
+tl.from("#section3", { xPercent: -100 })
+  .from("#section4", { xPercent: 100 })
+  .from("#section5", { yPercent: -100 });
+
+ScrollTrigger.create({
+  animation: tl,
+  trigger: ".wrapper",
+  start: "top top",
+  end: "+=4000",
+  scrub: true,
+  pin: true,
+  snap: 1 / 3,
+  anticipatePin: 1,
+});
