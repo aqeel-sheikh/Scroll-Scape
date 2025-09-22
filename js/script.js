b60f44ce61f5766e336be683b2f489ec4245f8fe
Aqeel <aqeel-sheikh@users.noreply.github.com>
2025-09-22 12:12:14 +0530

    refactor(section4): Enhance background, cards, narrative cards with improved styling and animations

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-const headerTitle = document.querySelector("#section1-title");
-const headerTagLine = document.querySelector("#section1-tagline");
-const socialLinks = document.querySelectorAll(".social-links a");
-const rocket = document.querySelector(".rocket");
-
-function onScroll() {
-  const scrollY = window.scrollY;
-
-  headerTitle.style.marginBottom = `${scrollY * 0.9}px`;
-
-  headerTagLine.style.marginTop = scrollY + "px";
-
-  for (let link of socialLinks) {
-    link.style.paddingBottom = `${scrollY * 0.9}px`;
-  }
-}
-
-window.addEventListener("scroll", onScroll, { passive: true });
-onScroll();
-
 gsap.registerPlugin(ScrollTrigger);
 gsap.defaults({ ease: "none", duration: 2 });
 
@@ -35,4 +15,40 @@ ScrollTrigger.create({
   pin: true,
   snap: 1 / 3,
   anticipatePin: 1,
+});
+
+// Section 1 - Header
+const headerTitle = document.querySelector("#section1-title");
+const headerTagLine = document.querySelector("#section1-tagline");
+const socialLinks = document.querySelectorAll(".social-links a");
+
+function onScroll() {
+  const scrollY = window.scrollY;
+
+  headerTitle.style.marginBottom = `${scrollY * 0.9}px`;
+
+  headerTagLine.style.marginTop = scrollY + "px";
+
+  for (let link of socialLinks) {
+    link.style.paddingBottom = `${scrollY * 0.9}px`;
+  }
+}
+
+window.addEventListener("scroll", onScroll, { passive: true });
+onScroll();
+
+// Section 4 - The Rise
+const card = document.getElementById("section4-narrative-card");
+
+card.addEventListener("mousemove", (e) => {
+  const rect = card.getBoundingClientRect();
+  const x = e.clientX - rect.left - rect.width;
+  const y = e.clientY - rect.top - rect.height;
+  const rotateY = (y / 15).toFixed(2);
+  const rotateX = (x / 15).toFixed(2);
+  card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+});
+
+card.addEventListener("mouseleave", () => {
+  card.style.transform = "rotateX(0deg) rotateY(0deg)";
 });
